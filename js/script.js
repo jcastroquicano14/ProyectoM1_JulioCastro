09dda8e7193aeb9100e69dd60cf09956d5f11054
Julio Castro <cq.julio.antonny@gmail.com>
2026-07-01 19:10:13 -0500

    js actualizado casi acabado

--- js/script.js
+++ js/script.js
@@ -35,10 +35,32 @@ function obtenerSizePaleta() {
 		document.querySelector('select[name="selector-cantidad"]').value,
 	);
 }
+//* Crear una funcion para indicar cuando marco el logo del candado que se genere una nueva paleta de colores, pero que no se modifique el color de la tarjeta bloqueada.
+
+function bloquearColor(tarjeta) {
+	const candado = tarjeta.querySelector(".candadoBtn");
+	tarjeta.classList.toggle("bloqueado");
+	if (tarjeta.classList.contains("bloqueado")) {
+		candado.textContent = "🔒";
+	} else {
+		candado.textContent = "🔓";
+	}
+}
 
 //* Generar la paleta de colores según la cantidad seleccionada por el usuario, y mostrarla en el contenedor correspondiente.
 
 function crearPaleta() {
+	const tarjetasExistentes =
+		contenedorColores.querySelectorAll(".tarjeta-color");
+	let coloresAnteriores = [];
+
+	tarjetasExistentes.forEach((tarjeta) => {
+		coloresAnteriores.push({
+			color: tarjeta.querySelector(".codigo-texto").textContent,
+			bloqueado: tarjeta.classList.contains("bloqueado"),
+		});
+	});
+
 	// Limpiamos el contenedor para borrar la paleta anterior
 	contenedorColores.innerHTML = "";
 
@@ -55,20 +77,42 @@ function crearPaleta() {
 	for (let i = 0; i < tamañoPaleta; i++) {
 		// 1. Decidimos qué color generar según el formato actual
 		const esHex = mostrarFormat.textContent === "Mostrar HEX";
-		const colorAleatorio = esHex ? generarHEX() : generarHSL();
+		let colorAleatorio;
+		let estaBloqueado = false;
+
+		if (coloresAnteriores[i] && coloresAnteriores[i].bloqueado) {
+			colorAleatorio = coloresAnteriores[i].color;
+			estaBloqueado = true;
+		} else {
+			colorAleatorio = esHex ? generarHEX() : generarHSL();
+		}
 
 		// 2. Creamos y configuramos el contenedor de la tarjeta
 		const tarjeta = document.createElement("div");
 		tarjeta.className = "tarjeta-color";
 		tarjeta.style.backgroundColor = colorAleatorio;
 
+		if (estaBloqueado) {
+			tarjeta.classList.add("bloqueado");
+		}
 		// 3. Creamos y configuramos el texto interno
 		const textoColor = document.createElement("span");
 		textoColor.className = "codigo-texto";
 		textoColor.textContent = colorAleatorio;
 
+		const candado = document.createElement("button");
+		candado.className = "candadoBtn";
+		candado.textContent = estaBloqueado ? "🔒" : "🔓";
+
+		candado.addEventListener("click", (e) => {
+			e.stopPropagation(); // Evita que el clic en el candado dispare el evento de la tarjeta
+
+			bloquearColor(tarjeta);
+		});
+
 		// 4. Inyectamos los elementos en el DOM
 		tarjeta.appendChild(textoColor);
+		tarjeta.appendChild(candado);
 		contenedorColores.appendChild(tarjeta);
 
 		// Agrega un evento a cada tarjeta de color para copiar el código al portapapeles y mostrar un aviso
